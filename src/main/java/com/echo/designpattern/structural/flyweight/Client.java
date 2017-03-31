c16a71e5d6a854948c273a75cc1ae4a1625b2f47
echo <echo940811@163.com>
2017-03-31 18:33:33 +0800

    shutdown exec

--- src/main/java/com/echo/designpattern/structural/flyweight/Client.java
+++ src/main/java/com/echo/designpattern/structural/flyweight/Client.java
@@ -10,14 +10,15 @@ import java.util.concurrent.Executors;
 public class Client {
 
     public static void main(String[] args) {
-        FontFactory factory = new FontFactory();
         int tc = 10;
         ExecutorService exec = Executors.newFixedThreadPool(tc);
+        FontFactory factory = new FontFactory();
         for (int i = 0; i < tc; i++) {
             exec.execute(() -> {
                 AbstractFont font = factory.get(FontFactory.Rendered.HELVETICA);
                 new Character("hello", 17, "red", font).show();
             });
         }
+        exec.shutdown();
     }
 }
